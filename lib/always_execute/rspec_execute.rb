89d52c120010116c6ffbf3a1c4dc47ccd045606b
Philippe Huibonhoa <phuibonhoa@gmail.com>
2014-10-31 01:35:41 -0700

    Allow execute to take a 'let' value to store the result of the execute block (otherwise store in execute_result)

--- lib/always_execute/rspec_execute.rb
+++ lib/always_execute/rspec_execute.rb
@@ -2,7 +2,7 @@ module RSpec
   module Core
     class Example
       alias :initialize_without_execute :initialize
-      
+
       def initialize(example_group_class, description, metadata, example_block=nil)
         if example_block
           example_block_with_execute = Proc.new do |*args|
@@ -12,16 +12,18 @@ module RSpec
             instance_exec(*args, &example_block)
           end
         end
-        
+
         initialize_without_execute(example_group_class, description, metadata, example_block_with_execute)
       end
     end
-    
+
     class ExampleGroup
-      def self.execute(&execute_block)
+      def self.execute(result_name=:execute_result, &execute_block)
         before do
           @execute_block = execute_block
         end
+
+        let(result_name) { @execute_result }
       end
     end
   end
--- spec/execute_spec.rb
+++ spec/execute_spec.rb
@@ -5,38 +5,46 @@ describe ".execute" do
     execute do
       :success
     end
-    
+
     it "should set execute_result to execute block return value" do
-      :success.should == @execute_result
+      :success.should == execute_result
     end
   end
-  
+
+  context "with execute_result_name" do
+    execute(:the_result) { :result_value }
+
+    it "stores execute result in variable with execute_result_name" do
+      the_result.should == :result_value
+    end
+  end
+
   context "execute in nested contexts" do
     before do
       @last_step = :before_level1
     end
-    
+
     execute do
       :before_level1.should == @last_step
       @last_step = :execute_level1
     end
-    
+
     it "should execute execute_level1" do
       @last_step.should == :execute_level1
     end
-    
+
     context "with nested context with no execute" do
       it "should execute execute_level1" do
         @last_step.should == :execute_level1
       end
     end
-    
+
     context "with nested context with execute override" do
       before do
         :before_level1.should == @last_step
         @last_step = :before_level2
       end
-      
+
       execute do
         :before_level2.should == @last_step
         @last_step = :execute_level2
@@ -47,4 +55,4 @@ describe ".execute" do
       end
     end
   end
-end  
+end

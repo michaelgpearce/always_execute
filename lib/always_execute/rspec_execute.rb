3a39fb417c4066b5544d4b98b9ff3d6ffafca939
Philippe Huibonhoa <phuibonhoa@gmail.com>
2015-04-24 02:47:07 -0700

    [BUGFIX] Compatibility with rspec 3.2 single run
    Newest rspec bases the single run off line number of the example group proc source_location.  Update the proc with execute to delegate the source_location to the original example block.

--- lib/always_execute/rspec_execute.rb
+++ lib/always_execute/rspec_execute.rb
@@ -3,9 +3,18 @@ module RSpec
     class Example
       alias :initialize_without_execute :initialize
 
+      class ProcWithDelegateSource < Proc
+        attr_reader :source_location
+
+        def initialize(delegate, &block)
+          @source_location = delegate.source_location
+          super(&block)
+        end
+      end
+
       def initialize(example_group_class, description, metadata, example_block=nil)
         if example_block
-          example_block_with_execute = Proc.new do |*args|
+          example_block_with_execute = ProcWithDelegateSource.new(example_block) do |*args|
             if @execute_block
               self.instance_variable_set('@execute_result', instance_exec(&@execute_block))
             end
